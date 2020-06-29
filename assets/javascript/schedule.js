df473246d559b4aec9dfb58b4515b347f6cca265
MROD <mriguez777@gmail.com>
2020-06-29 17:07:10 -0600

    Setting up file structure again and creating HTML for the app. Then configuring the Javascript code and getting it to work with firebase. Code is in place but firebase is not responding to data being sent. Need to review

---
+++ assets/javascript/schedule.js
@@ -0,0 +1,106 @@
+
+// Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAkTzd6dy9LhEUs1Ly7yIfO8hKTu9NsWGE",
+  authDomain: "train-schedule-c0549.firebaseapp.com",
+  databaseURL: "https://train-schedule-c0549.firebaseio.com",
+  projectId: "train-schedule-c0549",
+  storageBucket: "train-schedule-c0549.appspot.com",
+  messagingSenderId: "27093614698",
+  appId: "1:27093614698:web:42805db50db9097231ca13"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+// Grabs user input
+var trainName = $("#train-name-input")
+  .val()
+  .trim();
+var destination = $("#destination-input")
+  .val()
+  .trim();
+var firstTrain = $("#first-train-input")
+  .val()
+  .trim();
+var frequency = $("#frequency-input")
+  .val()
+  .trim();
+
+// Creates local "temporary" object for holding train data
+var newTrain = {
+  name: trainName,
+  destination: destination,
+  firstTrain: firstTrain,
+  frequency: frequency
+};
+
+// Uploads train data to the database
+trainData.ref().push(newTrain);
+
+// Logs everything to console
+console.log(newTrain.name);
+console.log(newTrain.destination);
+console.log(newTrain.firstTrain);
+console.log(newTrain.frequency);
+
+// Alert
+alert("Train successfully added");
+
+// Clears all of the text-boxes
+$("#train-name-input").val("");
+$("#destination-input").val("");
+$("#first-train-input").val("");
+$("#frequency-input").val("");
+});
+
+// 4. Create Firebase event for adding trains to the database and a row in the html when a user adds an entry
+trainData.ref().on("child_added", function (childSnapshot, prevChildKey) {
+  console.log(childSnapshot.val());
+
+  // Store everything into a variable.
+  var tName = childSnapshot.val().name;
+  var tDestination = childSnapshot.val().destination;
+  var tFrequency = childSnapshot.val().frequency;
+  var tFirstTrain = childSnapshot.val().firstTrain;
+
+  var timeArr = tFirstTrain.split(":");
+  var trainTime = moment()
+    .hours(timeArr[0])
+    .minutes(timeArr[1]);
+  var maxMoment = moment.max(moment(), trainTime);
+  var tMinutes;
+  var tArrival;
+
+  // If the first train is later than the current time, sent arrival to the first train time
+  if (maxMoment === trainTime) {
+    tArrival = trainTime.format("hh:mm A");
+    tMinutes = trainTime.diff(moment(), "minutes");
+  } else {
+    // Calculate the minutes until arrival using hardcore math
+    // To calculate the minutes till arrival, take the current time in unix subtract the FirstTrain time
+    // and find the modulus between the difference and the frequency.
+    var differenceTimes = moment().diff(trainTime, "minutes");
+    var tRemainder = differenceTimes % tFrequency;
+    tMinutes = tFrequency - tRemainder;
+    // To calculate the arrival time, add the tMinutes to the current time
+    tArrival = moment()
+      .add(tMinutes, "m")
+      .format("hh:mm A");
+  }
+  console.log("tMinutes:", tMinutes);
+  console.log("tArrival:", tArrival);
+
+  // Add each train's data into the table
+  $("#train-table > tbody").append(
+    $("<tr>").append(
+      $("<td>").text(tName),
+      $("<td>").text(tDestination),
+      $("<td>").text(tFrequency),
+      $("<td>").text(tArrival),
+      $("<td>").text(tMinutes)
+    )
+  );
+});
+
